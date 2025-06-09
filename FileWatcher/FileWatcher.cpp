5cf09e24a4a067eaf12bd0a95c025afe10e52c7c
AndrDia <crazycucumber@DESKTOP-COOBG5R.localdomain>
2025-06-09 22:03:17 +0300

    [FileWatcher] propagator added

--- FileWatcher/FileWatcher.cpp
+++ FileWatcher/FileWatcher.cpp
@@ -1,21 +1,25 @@
 #include "FileWatcher.hpp"
 #include <boost/asio/post.hpp>
 
-FileWatcher::FileWatcher(boost::asio::io_context &io, EventCallback cb)
-    : io_(io),
-      callback_(std::move(cb)),
-      watcher_(std::make_unique<efsw::FileWatcher>(true)) {  // generic
+FileWatcher::FileWatcher(ChangePropagator::ChangePropagator &prop, boost::asio::io_context &io, EventCallback cb)
+    : prop_(prop)
+    , io_(io)
+    , callback_(std::move(cb))
+    , watcher_(std::make_unique<efsw::FileWatcher>(true))
+{ // generic
 }
 
 FileWatcher::~FileWatcher() = default;
 
-void FileWatcher::run() const {
+void FileWatcher::run() const
+{
     watcher_->watch();
 }
 
-void FileWatcher::addWatch(const std::string &path) {
+void FileWatcher::addWatch(const std::string &path)
+{
     namespace fs = std::filesystem;
-    const fs::path p{path};
+    const fs::path p { path };
 
     auto addDir = [&](const std::string &dir) {
         if (watched_dirs.insert(dir).second) {
@@ -46,16 +50,17 @@ void FileWatcher::addWatch(const std::string &path) {
     }
 }
 
-FW_Event::Type FileWatcher::to_Event(const efsw::Action action) {
+FW_Event::Type FileWatcher::to_Event(const efsw::Action action)
+{
     switch (action) {
-        case efsw::Actions::Add:
-            return FW_Event::Type::Added;
-        case efsw::Actions::Delete:
-            return FW_Event::Type::Deleted;
-        case efsw::Actions::Modified:
-            return FW_Event::Type::Modified;
-        case efsw::Actions::Moved:
-            return FW_Event::Type::Moved;
+    case efsw::Actions::Add:
+        return FW_Event::Type::Added;
+    case efsw::Actions::Delete:
+        return FW_Event::Type::Deleted;
+    case efsw::Actions::Modified:
+        return FW_Event::Type::Modified;
+    case efsw::Actions::Moved:
+        return FW_Event::Type::Moved;
     }
     return FW_Event::Type::Modified;
 }
@@ -66,59 +71,94 @@ void FileWatcher::handleFileAction(
     const std::string &filename,
     const efsw::Action action,
     const std::string oldFilename
-) {
+)
+{
     namespace fs = std::filesystem;
 
-    const std::string oldFull =
-        fs::absolute(fs::path(dir) / oldFilename).string();
+    const std::string oldFull = fs::absolute(fs::path(dir) / oldFilename).string();
     const std::string newFull = fs::absolute(fs::path(dir) / filename).string();
 
-    FW_Event event;
-    event.type = to_Event(action);
-    event.new_path = newFull;
+    FW_Event event { to_Event(action), {}, {} };
 
     if (action == efsw::Actions::Moved) {
-        if (watched_files.erase(oldFull)) {
-            event.old_path = oldFull;
+        if (watched_files.contains(oldFull)) {
+            event.old_paths.push_back("\"" + oldFull + "\"");
+            event.new_paths.push_back("\"" + newFull + "\"");
+            watched_files.erase(oldFull);
             watched_files.insert(newFull);
-        } else if (watched_dirs.erase(oldFull)) {
-            event.old_path = oldFull;
+        } else if (watched_dirs.contains(oldFull)) {
 
-            const std::string prefix = oldFull + std::string(1, fs::path::preferred_separator);
-            for (auto it = watched_dirs.begin(); it != watched_dirs.end();) {
-                const std::string &d = *it;
-                if (d == oldFull || d.rfind(prefix, 0) == 0) {
-                    watcher_->removeWatch(d);
-                    it = watched_dirs.erase(it);
-                } else {
-                    ++it;
+            const char sep = static_cast<char>(fs::path::preferred_separator);
+            const std::string oldPref = oldFull + std::string(1, sep);
+            const std::string newPref = newFull + std::string(1, sep);
+            // const std::string oldPref = oldFull + fs::path::preferred_separator;
+            // const std::string newPref = newFull + fs::path::preferred_separator;
+
+            std::vector<std::string> dirsToRemove;
+            dirsToRemove.push_back(oldFull);
+            for (auto &d : watched_dirs) {
+                if (d.rfind(oldPref, 0) == 0) {
+                    dirsToRemove.push_back(d);
                 }
             }
 
-            for (auto it = watched_files.begin(); it != watched_files.end();) {
-                if (it->rfind(prefix, 0) == 0) {
-                    it = watched_files.erase(it);
-                } else {
-                    ++it;
+            std::vector<std::string> filesToRemove;
+            for (auto &f : watched_files) {
+                if (f.rfind(oldPref, 0) == 0) {
+                    filesToRemove.push_back(f);
                 }
             }
-            addWatch(newFull);
+
+            for (auto &oldD : dirsToRemove) {
+                std::string newD = (oldD == oldFull ? newFull : newPref + oldD.substr(oldPref.size()));
+                watched_dirs.erase(oldD);
+                watched_dirs.insert(newD);
+            }
+            for (auto &oldF : filesToRemove) {
+                std::string newF = newPref + oldF.substr(oldPref.size());
+                watched_files.erase(oldF);
+                watched_files.insert(newF);
+                event.old_paths.push_back("\"" + oldF + "\"");
+                event.new_paths.push_back("\"" + newF + "\"");
+            }
         } else {
             return;
         }
     } else if (action == efsw::Actions::Delete) {
-        if (!watched_files.erase(newFull)) {
+        if (!watched_files.contains(newFull)) {
             return;
         }
+        event.old_paths.push_back("\"" + newFull + "\"");
+        watched_files.erase(newFull);
     } else if (action == efsw::Actions::Modified) {
         if (!watched_files.contains(newFull)) {
             return;
         }
-    } else if (action == efsw::Actions::Add) {
-        return;
+        event.new_paths.push_back("\"" + newFull + "\"");
+    } else {
+        return; // игнор Add
     }
 
-    boost::asio::post(io_, [cb = callback_, event = std::move(event)]() {
-        cb(event);
+    boost::asio::post(io_, [this, ev = std::move(event)]() mutable {
+        if (ev.type == FW_Event::Type::Deleted) {
+            for (auto &oldp : ev.old_paths) {
+                std::vector<std::string_view> parts { "delete_local_file", oldp };
+                boost::asio::co_spawn(io_, prop_.on_local_change(parts), boost::asio::detached);
+            }
+        } else if (ev.type == FW_Event::Type::Modified) {
+            for (auto &newp : ev.new_paths) {
+                std::vector<std::string_view> parts { "update_file", newp };
+                boost::asio::co_spawn(io_, prop_.on_local_change(parts), boost::asio::detached);
+            }
+        } else if (ev.type == FW_Event::Type::Moved) {
+            for (size_t i = 0; i < ev.old_paths.size(); ++i) {
+                std::vector<std::string_view> parts {
+                    "update_local_path",
+                    ev.old_paths[i],
+                    ev.new_paths[i]
+                };
+                boost::asio::co_spawn(io_, prop_.on_local_change(parts), boost::asio::detached);
+            }
+        }
     });
 }
